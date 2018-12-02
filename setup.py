7825e42972ddc9d73fc5684e6d5e232cff7ae184
John  Conway <JPatrick9793@gmail.com>
2018-12-02 11:28:00 -0500

    third commit

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/JPatrick9793/test_project.git'
 EMAIL = 'john.conway@novaiq.io'
 AUTHOR = 'John Conway'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0'
+VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
